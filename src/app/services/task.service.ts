4d91ab8a26b256aa1282df6643cf5333b5f3be1e
berdnaski <berdnaski1177@gmail.com>
2025-12-05 11:06:52 -0300

    feat: adding interfaces to task

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,16 +1,17 @@
 import { Injectable } from "@angular/core";
 import { BehaviorSubject } from "rxjs";
+import { ITask } from "../interfaces/task.interface";
 
 @Injectable({
   providedIn: 'root',
 })
 export class TaskService {
-  private todoTasks$ = new BehaviorSubject<any[]>([]);
+  private todoTasks$ = new BehaviorSubject<ITask[]>([]);
   readonly todoTasks = this.todoTasks$.asObservable();
 
-  private doingTasks$ = new BehaviorSubject<any[]>([]);
+  private doingTasks$ = new BehaviorSubject<ITask[]>([]);
   readonly doingTasks = this.doingTasks$.asObservable();
 
-  private doneTasks$ = new BehaviorSubject<any[]>([]);
+  private doneTasks$ = new BehaviorSubject<ITask[]>([]);
   readonly doneTasks = this.doneTasks$.asObservable();
 }
