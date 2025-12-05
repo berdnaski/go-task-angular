9899b686b8693e821ebab4266e05384cce76c1c5
berdnaski <berdnaski1177@gmail.com>
2025-12-04 23:48:37 -0300

    feat: adding layout comments tasks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/components/header/header.component";
 import { MainContentComponent } from "./components/components/main-content/main-content.component";
 import { TaskFormModalComponent } from "./components/components/task-form-modal/task-form-modal.component";
+import { TaskCommentsModalComponent } from "./components/components/task-comments-modal/task-comments-modal.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, MainContentComponent, TaskFormModalComponent],
+  imports: [RouterOutlet, HeaderComponent, MainContentComponent, TaskFormModalComponent, TaskCommentsModalComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
